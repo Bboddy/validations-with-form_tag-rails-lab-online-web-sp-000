576b25e710ad0af229bff36d8fed296df66d73a3
Bryce B <bryce.boddy@gmail.com>
2021-06-15 02:12:11 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,40 @@
+
 class PostsController < ApplicationController
-  def show
-    @post = Post.find(params[:id])
-  end
+def show
+  @post = Post.find(params[:id])
+end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+def new
+  @post = Post.new
+end
 
-  def update
-    @post = Post.find(params[:id])
+def create
+  @post = Post.new(post_params)
 
-    @post.update(post_params)
-
+  if @post.save
     redirect_to post_path(@post)
+  else
+    render :new
   end
+end
 
-  private
+def edit
+  @post = Post.find(params[:id])
+end
 
-  def post_params
-    params.permit(:title, :category, :content)
+def update
+  @post = Post.find(params[:id])
+
+  if @post.update(post_params)
+    redirect_to post_path(@post)
+  else
+    render :edit
   end
+end
+
+private
+
+def post_params
+  params.permit(:title, :category, :content)
+end
 end
